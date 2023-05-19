7daaf37520fc38b95c1f3b918f9a068f68b00c0d
Gustavo Borges <63672758+GustavoBorges13@users.noreply.github.com>
2023-05-19 04:38:33 +0000

    Pending changes exported from your codespace

--- index.php
+++ index.php
@@ -1,27 +1,77 @@
 <?php
+//Comando trocadilho by Gustavo Borges (AslordX) - Twitch Radull [https://www.twitch.tv/radull]
+#Versão 1.4 (Data de atualização: 20/04/2021 | Horário: 9:43h)
 
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
+$t1 = "Eu tô SHEN SHACO pra isso!";
+$t2 = "ZOE essa lane!";
+$t3 = "Minha MAOKAI quando eu vejo esse Campeão.";
+$t4 = "URGOTTA be kidding me!";
+$t5 = "Bebi muita MORDEKAISER hoje.";
+$t6 = "Eu odeio POPPYS! (Poppy também se refere a uma flor).";
+$t7 = "SUSAN!! (É o nome do Nasus ao contrário, kk).";
+$t8 = "Essa lane tá tão calma que só escuto meu EKKO.";
+$t9 = "Mas o que foi que eu FIZZ?";
+$t10 = "Eu VI isso?!";
+$t11 = "Meu GALIO tá grande hoje!.";
+$t12 = "JAX chega!";
+$t13 = "Isso é EZREAL?";
+$t14 = "Você pode não estar RINDO da LIVE agora, mas o EZREAL.";
+$t15 = "Meu MUNDO tá de cabeça pra baixo!";
+$t16 = "Essa mulher é uma LEONA!";
+$t17 = "Ai meu LULU!";
+$t18 = "Cuidado com o vírus KHA'ZIX!";
+$t19 = "Li o AURELION no SOL.";
+$t20 = "ASHE ou não ASHE, eu ASHE isso certo!";
+$t21 = "DARIUS uma surra!";
+$t22 = "JANNAsceu?";
+$t23 = "Vo te bate com minha VARUS.";
+$t24 = "Tu TARICo ein?";
+$t25 = "Consegui meu TALON de cheque!";
+$t26 = "TEEMO que isso é nada bom!";
+$t27 = "XERATH que isso vai dar certo?";
+$t28 = "AHRI que dor!";
+$t29 = "Eu pago com dollar e o Ezreal.";
+$t30 = "O que o ZED foi fazer na PRAIA? <=> Foi andar de ZED SKI";
+$t31 = "Por que Janna e Yasuo counteram nasus? <=> Porque eles tem o fura-cão";
+$t32 = "Por que o kha'zix tem asas? <=> Porque ele é jungle, e red e blue te da asas (REDBULL...).";
+$t33 = "Por que o Garen pega todas? <=> Porque ele é um garen nhão";
+$t34 = "Qual o campeão que gruda nos lugares? <=> O Vel'croz (velcro)";
+$t35 = "Qual o suporte com o melhor roaming do jogo? <=> Alistar porque onde voce estiver ele ALIESTAR.";
+$t36 = "Por que o Yasuo consegue invadir qualquer casa?  <=> Porque ele HAS A KEY.";
+$t37 = "Qual o adc que countera o Pyke?  <=> Aquela que ta KAllista dele (Com a lista).";
+$t38 = "Por que o Lucian não pode ser ator?  <=> Porque ele perdeu a Senna.";
+$t39 = "Por que o  range da barrigada do gragas é alto? Sei lá também mas que boneco roubado!!!!";
+$t40 = "Qual Árvore é uma grande derrubadora do Mal?  <=> Maokai (Mal Cai).";
+$t41 = "Qual Campeão não vale nem um real?  <=> Shentavo.";
+$t42 = "Sabe qual a runa que te faz pegar todas? <=> Conquistador";
+$t43 = "Você daqui e Nidalee.";
+$t44 = "O Lee Sin era cego, já a Anivia.";
+$t45 = "Lux: — Hi car! | — car: hilux!";
+$t46 = "Ainda bem que a Lux é de luz, pois se fosse de eletricidade ela se chamaria: eletroLUX!";
+$t47 = "Eu entro e o rek'sai.";
+$t48 = "a Irelia.. agora n lê mais ;-;";
+$t49 = "O bem sobe e o Maokai.";
+$t50 = "Como o Lucian mata as pessoas? Ka arma.";
+$t51 = "Eu tô SHEN SHACO pra isso!";
+$t52 = "Por que o alistar vaquinha foi para o espaço? <=> Para se encontrar com o vácuo.";
+$t53 = "Fi está no quarto, pois FiORA";
 
-function sayHello($name) {
-	echo "Hello $name!";
-}
+$rt = array($t1, $t2, $t3, $t4, $t5, $t6, $t7, $t8, $t9, $t10, $t11, $t12, $t13, $t14, $t15, $t16, $t17, $t18, $t19, $t20, $t21, $t22, $t23, $t24, $t25, $t26, $t27, $t28, $t29, $t30, $t31, $t32, $t33, $t34, $t35, $t36, $t37, $t38, $t39, $t40, $t41, $t42, $t43, $t44, $t45, $t46, $t47, $t48, $t49, $t50, $t51, $t52, $t53); //Aqui ele agrupa todos os trocadilhos em um grupo.
+$chaves_aleatorias = array_rand($rt, 1); //Aqui vai selecionar 1 trocadilho aleatorio presente naquele grupo.
+echo $rt[$chaves_aleatorias];
 
-?>
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ATENÇÃO ATENÇÃO ATENÇÃO ATENÇÃO ATENÇÃO !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#Observações>
+#Para o nightbot use: !addcom !trocadilho $(user) fez o seguinte trocadilho: $(urlfetch http://radullrepositorio.medianewsonline.com/ComandoTROCADILHO.php)
+#Para o StreamElements use: !command add !trocadilho $(user) fez o seguinte trocadilho: $(urlfetch http://radullrepositorio.medianewsonline.com/ComandoTROCADILHO.php)
 
-<html>
-	<head>
-		<title>Visual Studio Code Remote :: PHP</title>
-	</head>
-	<body>
-		<?php 
-		
-		sayHello('remote world');
-			
-		phpinfo(); 
-			
-		?>
-	</body>
-</html>
+/* CREDITOS AOS TROCADILHOS (FONTES DE PESQUISA) > 
+Lista de trocadilhos do lol por EQG Greengu, disponível no link:
+- https://forums.comunidades.riotgames.com/t5/Outros-Assuntos-de-LoL/MINHA-LISTA-DE-TROCADILHOS-DE-CAMPEÕES-DO-LoL/td-p/43481
+Outros trocadilhos aleatorios foram feitos por: Teyas, O Ultimo Samurai, CarIosHoltz, TioKilllua, Bikman e samucão123 disponível no seguinte link:
+- https://forums.comunidades.riotgames.com/t5/Memes-e-Piadas/Algumas-piadas-de-lol/td-p/513626
+Outros trocadilhos randoms (sem ser de lol), disponíveis em:
+- https://www.maioresemelhores.com/trocadilhos-mais-bobos-e-engracados/
+- https://buzzfeed.com.br/post/as-14-piadas-horriveis-mais-engracadas-do-brasil
+*/
+?> 
