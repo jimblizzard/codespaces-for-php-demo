0f8820befbfb5ab995590b1f6bcbbfdb1c2a63ad
tobji <63978904+tobjiMoh@users.noreply.github.com>
2023-12-06 11:10:47 +0100

    Update index.php
    test connexion bdd tobji

--- index.php
+++ index.php
@@ -1,27 +1,18 @@
 <?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', '51.210.151.13');
+define('DB_USERNAME', 'EleveRostand1!');
+define('DB_PASSWORD', 'EleveRostand1!');
+define('DB_NAME', 'tobji');
 
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
-
-function sayHello($name) {
-	echo "Hello $name!";
+/* Attempt to connect to MySQL database */
+try{
+    $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
+    // Set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	echo "connexion réussie";
+} catch(PDOException $e){
+    die("ERROR: Could not connect. " . $e->getMessage());
 }
-
 ?>
-
-<html>
-	<head>
-		<title>Visual Studio Code Remote :: PHP</title>
-	</head>
-	<body>
-		<?php 
-		
-		sayHello('remote world');
-			
-		phpinfo(); 
-			
-		?>
-	</body>
-</html>
